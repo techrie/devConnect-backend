c00d0d1c47f0f563686c8a901cf8a091ff435c19
techrie <cute.n.charming@gmail.com>
2026-06-14 21:50:33 -0700

    updated DB

--- src/config/database.js
+++ src/config/database.js
@@ -2,18 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://poojitham255:U9wM94SUnnBCUdti@nodecourse.id077.mongodb.net/devConnect",
-  );
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 module.exports = connectDB;
-
-// mongodb+srv://poojitham255:<db_password>@nodecourse.id077.mongodb.net/NodeCourse
-
-//Notes
-
-//Go to mongodb website
-// Create a free cluster M0 cluster on mongodb atlas
-// create a user to access to mongodb atlas
-// Get the connection string
-// Install mongodb compass
